0bd1365ae50359fbe79196460259d8b57cd6be0a
wuxiaofang <wuxiaofang@yongche.com>
2016-09-21 15:24:03 +0800

    commit all

--- WWProject/AppDelegate.m
+++ WWProject/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <WWHttpModule/WWHttpClient.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    WWHttpClient* httpClient = [[WWHttpClient alloc] init];
+    [httpClient printfClassInfo];
     return YES;
 }
 
